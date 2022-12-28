606ba8ee6480d2ba23e77a06a469c4120d36bef0
Abe Handler <akh2103@gmail.com>
2022-12-28 07:17:00 -0700

    postproc

--- ebikes/upwork_post_processor.py
+++ ebikes/upwork_post_processor.py
@@ -1,15 +1,37 @@
 import requests
 import re
-import nltk
+import spacy
+from spacy.lang.en import English
 from bs4 import BeautifulSoup
 import gzip
 import json  
 import docxpy
+import numpy as np
 from dataclasses import dataclass
+from numpy import ndarray
 from typing import List
 from colorama import Fore
+from sentence_transformers import SentenceTransformer
 from colorama import Style
 
+
+def get_ngrams(tokens, n=2):
+    return [" ".join(o) for o in zip(*[tokens[i:] for i in range(n)])]
+
+
+class Spacy(object):
+
+    def __init__(self, model = "en_core_web_sm"):
+        self.nlp = spacy.load(model)
+
+    def tokenize(self, text):
+        doc = self.nlp(text)
+        tokens = [str(t) for t in doc]
+        return tokens
+
+    def sentence_split(self, text):
+        '''this seems to be too restrictive for reddit comments'''
+        return [str(i) for i in self.nlp(text).sents]
 
 class DocX(object):
 
@@ -25,26 +47,6 @@ class DocX(object):
         hyperlinks = doc.data['links']
 
         return hyperlinks
-
-class Webpage2Text(object):
-        
-    def get_text(self, url='https://yubabikes.com/cargobikestore/mundo-electric/'):
-        r = requests.get(url)
-        soup = BeautifulSoup(r.text, 'html.parser')
-        text = soup.text.replace("\n", " ").replace("\t", " ").strip()
-        text = re.sub(' +', ' ', text)
-        return text
-
-
-class Tokenizer(object):
-    
-    @staticmethod
-    def tokenize(text):
-        return nltk.word_tokenize(text)
-    
-    @staticmethod
-    def get_ngrams(tokens, n=2):
-        return [" ".join(o) for o in zip(*[tokens[i:] for i in range(n)])]
 
 
 class Reddit(object):
@@ -64,66 +66,80 @@ class Reddit(object):
         return out
 
     @staticmethod
-    def get_comments_mentioning_product_and_flatten(comments, product):
+    def get_comments_mentioning_product(comments, product):
         out = []
         for o in comments:
             if product in o["body"]:
                 out.append(o["body"])
-        return " ".join(out)
-    
+        return out
+
+
 @dataclass
-class URL:
+class WebpageText:
     url: str
-    tokens: List[str]
-    ngrams: List[str]
+    page_text: str
+    page_sentences: List[str]
+    sentence_embeddings: ndarray
+
+
+class WebpageFactory(object):
+
+    def __init__(self, spacy, sbert):
+        self.spacy = spacy
+        self.sbert = sbert
+
+    def make_webpage(self, url='https://yubabikes.com/cargobikestore/mundo-electric/'):
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        text = soup.text.replace("\n", " ").replace("\t", " ").strip()
+        text = re.sub(' +', ' ', text)
+        page_sentences = list(self.spacy.sentence_split(text))
+        page_sentence_embeddings = sbert.encode(page_sentences)
+        return WebpageText(url, text, page_sentences, page_sentence_embeddings)
 
 
 @dataclass
 class ProductMention:
-    url: URL
-    text: str
-    comments: List[dict]
-    ngrams: List[str]
+    comment_containing_mention: str
+    sentence_containing_mention: str
+
+
+if __name__ == "__main__":
+    docx = DocX()
+    hyperlinks = docx.get_links()
+    links = hyperlinks[5:9]
+
+    sbert = SentenceTransformer('all-MiniLM-L6-v2')
+    reddit = Reddit()
+
+    print('\033[39m')
+    print(Fore.RED + 'Warning: The post yaz4wv is hard coded.')
+    print(Fore.YELLOW + 'Idea: To validate work, see if they get the same links as included in post.')
+    print(Fore.WHITE + "")
+
+    comments_for_link = reddit.get_comments_for_link_id("yaz4wv")
+
+    spacy = Spacy()
+    factory = WebpageFactory(spacy=spacy,
+                             sbert=sbert)
+
+    for product, url in links[1:]:
+
+        webpage = factory.make_webpage(url)
+        product = product.decode()
+        comments_about_product: List[str] = reddit.get_comments_mentioning_product(comments_for_link, product=product)
         
-class Processor(object):
-    
-    def __init__(self, w2t: Webpage2Text, tok: Tokenizer):
-        self.w2t = Webpage2Text()
-        self.tok = Tokenizer()
-    
-    def process_url_string_to_object(self, url, N: int=2):
-        text = self.w2t.get_text(url)
-        tokens = self.tok.tokenize(text)
-        ngrams = self.tok.get_ngrams(tokens, N)
-        return URL(url, tokens, ngrams)
+        comment_sentence_embeddings = sbert.encode(comments_about_product)
 
-nltk.download('punkt', quiet=True)
+        if len(comments_about_product) > 1:
+            sims = comment_sentence_embeddings.dot(webpage.sentence_embeddings.T)
+            sorted_ix = np.argsort(sims, axis=1) # get top 5 most similar
+            
+            for commentno, comment in enumerate(comments_about_product):
+                ranked_sents = sorted_ix[commentno][-5:]
+                print("***")
+                print(comment)
+                for r in ranked_sents:
+                    print(webpage.page_sentences[r])
+    print(Fore.YELLOW + 'Idea: Also check bigram overlap')
 
-docx = DocX()
-hyperlinks = docx.get_links()
-links = hyperlinks[5:9]
-
-reddit = Reddit()
-
-print('\033[39m')
-print(Fore.RED + 'Warning: The post yaz4wv is hard coded.')
-print(Fore.WHITE + "")
-
-comments = reddit.get_comments_for_link_id("yaz4wv")
-
-w2t = Webpage2Text()
-tokenizer = Tokenizer()
-processor = Processor(w2t, tokenizer)
-
-
-for product, url in links[1:]:
-    product = product.decode()
-    url = processor.process_url_string_to_object(url)
-    comments_about_product_flattend: str = reddit.get_comments_mentioning_product_and_flatten(comments, product=product)
-    tok = tokenizer.tokenize(comments_about_product_flattend)
-    reddit_ngrams = tokenizer.get_ngrams(tokens=tok)
-    mention = ProductMention(url, product, comments_about_product_flattend, reddit_ngrams)
-
-    print("****")
-    print(product)
-    print(set(url.ngrams) & set(reddit_ngrams))
